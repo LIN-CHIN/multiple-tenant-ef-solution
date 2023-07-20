86f56d56706eb1df97539e0ae9ee0201a1ce658f
LIN-CHIN <s235068267@gmail.com>
2023-07-20 15:44:00 +0800

    feat: add the capability to catch global errors.

---
+++ multiple-tenant-solution/multiple-tenant-solution/Controllers/ErrorController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using multiple_tenant_solution.Responses;
+using Npgsql;
+
+namespace multiple_tenant_solution.Controllers
+{
+    /// <summary>
+	/// 處理所有Exception的controller，此controller會在Program.cs由UseExceptionHandler註冊
+	/// </summary>
+	[Route("api/[controller]")]
+    [ApiController]
+    [ApiExplorerSettings(IgnoreApi = true)]
+    public class ErrorController : ControllerBase
+    {
+        /// <summary>
+        /// 處理所有Exception的controller constructor，IoC container會由此注入
+        /// </summary>
+        /// <param name="logger"></param>
+        public ErrorController()
+        {
+        }
+
+        /// <summary>
+        /// Error handling
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult<ApiResponse<string>> Error()
+        {
+            var context = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            var exception = context!.Error;
+
+            ApiResponse<string> apiErrorMessage = new ApiResponse<string>();
+
+            if (exception.GetBaseException() is PostgresException)
+            {
+                apiErrorMessage = HandlePostgresException((PostgresException)exception.GetBaseException());
+            }
+            else
+            {
+                apiErrorMessage = new ApiResponse<string>()
+                {
+                    Code = 10001,
+                    Message = "系統錯誤",
+                    Content = exception.ToString()
+                };
+            }
+
+            return StatusCode(StatusCodes.Status200OK, apiErrorMessage);
+        }
+
+        /// <summary>
+        /// 專門處理PostgresException
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        private ApiResponse<string> HandlePostgresException(PostgresException exception)
+        {
+            ApiResponse<string> apiErrorMessage = new ApiResponse<string>();
+
+            if (exception.SqlState == "23505")
+            {
+                apiErrorMessage = new ApiResponse<string>()
+                {
+                    Code = 20001,
+                    Message = "資料庫key相關的錯誤",
+                    Content = exception.ToString()
+                };
+            }
+            else
+            {
+                apiErrorMessage = new ApiResponse<string>()
+                {
+                    Code = 20000,
+                    Message = "資料庫相關的錯誤",
+                    Content = exception.ToString()
+                };
+            }
+            return apiErrorMessage;
+        }
+    }
+}
--- multiple-tenant-solution/multiple-tenant-solution/Program.cs
+++ multiple-tenant-solution/multiple-tenant-solution/Program.cs
@@ -9,54 +9,64 @@ using multiple_tenant_solution.Services.User;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Configuration.AddEnvironmentVariables();
-builder.Services.AddHttpClient();
-
-var apiSettings = builder.Configuration.GetSection("ApiSettings")
-    .Get<ApiSettings>(c => c.BindNonPublicProperties = true);
-
-var tenantServiceConfigs = builder.Configuration.GetSection("TenantServiceConfigs")
-    .Get<TenantServiceConfigs>(c => c.BindNonPublicProperties = true);
-
-// inject services 
-builder.Services.AddSingleton(apiSettings);
-builder.Services.AddSingleton(tenantServiceConfigs);
-builder.Services.AddScoped<CurrentUserInfo>();
-builder.Services.AddDbContext<DataContext>();
-builder.Services.AddTransient<IMaterialService, MaterialService>();
-builder.Services.AddTransient<IMaterialDAO, MaterialDAO>();
-builder.Services.AddTransient<IUserService, UserService>();
-builder.Services.AddTransient<IUserDAO, UserDAO>();
-
-// inject external services 
-builder.Services.AddTransient<ITenantExternalService, TenantExternalService>();
-
-var app = builder.Build();
-
-//db migrate
-using (var scope = app.Services.CreateScope())
+try
 {
-    var services = scope.ServiceProvider;
-    var context = services.GetRequiredService<DataContext>();
-    context.Database.Migrate();
-}
+    // Add services to the container.
+    builder.Services.AddControllers();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Configuration.AddEnvironmentVariables();
+    builder.Services.AddHttpClient();
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
+    var apiSettings = builder.Configuration.GetSection("ApiSettings")
+        .Get<ApiSettings>(c => c.BindNonPublicProperties = true);
+
+    var tenantServiceConfigs = builder.Configuration.GetSection("TenantServiceConfigs")
+        .Get<TenantServiceConfigs>(c => c.BindNonPublicProperties = true);
+
+    // inject services 
+    builder.Services.AddSingleton(apiSettings);
+    builder.Services.AddSingleton(tenantServiceConfigs);
+    builder.Services.AddScoped<CurrentUserInfo>();
+    builder.Services.AddDbContext<DataContext>();
+    builder.Services.AddTransient<IMaterialService, MaterialService>();
+    builder.Services.AddTransient<IMaterialDAO, MaterialDAO>();
+    builder.Services.AddTransient<IUserService, UserService>();
+    builder.Services.AddTransient<IUserDAO, UserDAO>();
+
+    // inject external services 
+    builder.Services.AddTransient<ITenantExternalService, TenantExternalService>();
+
+    var app = builder.Build();
+
+    app.UseExceptionHandler("/api/Error");
+
+    //db migrate
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<DataContext>();
+        context.Database.Migrate();
+    }
+
+    // Configure the HTTP request pipeline.
+    //if (app.Environment.IsDevelopment())
+    //{
     app.UseSwagger();
     app.UseSwaggerUI();
-//}
+    //}
 
-//app.UseHttpsRedirection();
+    //app.UseHttpsRedirection();
 
-app.UseAuthorization();
+    app.UseAuthorization();
 
-app.MapControllers();
+    app.MapControllers();
 
-app.Run();
+    app.Run();
+}
+catch (Exception ex) 
+{
+    Console.WriteLine(ex.ToString());
+    throw ex;
+}
